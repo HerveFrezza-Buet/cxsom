7b886a36f3d6e5462bf54ffc3263767498c09a56
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2021-11-23 23:52:36 +0100

    updt

--- cxsom/spec/notations.py
+++ cxsom/spec/notations.py
@@ -21,7 +21,31 @@ blocked  = tag('blocked')
 relaxing = tag('relaxing')
 checking = tag('checking')
 
-                             
+def DIof(T, X, t):
+    return nb.math.matrix(nb.seq(T, X, t))
+def RDIof(T, X, t):
+    return nb.math.brace(nb.seq(T, X, t))
+
+T, X, t, dt = nb.to('T X t \\tau')
+DI = DIof(T, X, t)
+RDI = RDIof(T, X, t)
+
+time_step = nb.symbol('{\\cal S}')
+TS = time_step@(T, t)
+
+def TSQ(ts, queue):
+    return nb.math.matrix(ts)@nb.rm(queue)
+TSQu = TSQ(TS, 'unstable')
+TSQi = TSQ(TS, 'impossible')
+TSQs = TSQ(TS, 'stable')
+TSQc = TSQ(TS, 'confirmed')
+
+def datation(x):
+    return nb.rm('d')@nb.to(x)
+
+def status(x):
+    return nb.math.bracket(x)
+
 with nb.files.defs('commands.tex') as defs:
     nb.config.push('display style', True) # We set the displaystyle
     defs.prefix = 'cx'
@@ -38,6 +62,17 @@ with nb.files.defs('commands.tex') as defs:
     defs['Blocked']    = blocked
     defs['Relaxing']   = relaxing
     defs['Checking']   = checking
+
+    defs['DftDI'] = DI
+    defs['DftRDI'] = RDI
+    defs['DftTS'] = TS
+
+    defs['DftTSQu'] = TSQu
+    defs['DftTSQi'] = TSQi
+    defs['DftTSQs'] = TSQs
+    defs['DftTSQc'] = TSQc
+
+    defs['DefDatation'] = nb.sets.isin(datation(DI), nb.sets.N)
 
     defs.add_preamble('\\usepackage{color}')
     defs.add_preamble('\\definecolor{funcoul}{rgb}{0,0,0.75}')
