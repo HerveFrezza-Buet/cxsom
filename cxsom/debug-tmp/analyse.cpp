194e545b656acc4f92e64c719a1ecad91a126007
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2023-01-16 16:26:39 +0100

    updt

--- cxsom/debug-tmp/analyse.cpp
+++ cxsom/debug-tmp/analyse.cpp
@@ -1,9 +1,11 @@
 #include <cxsom-viewer.hpp>
 
 #include <string>
+#include <fstream>
 #include <iostream>
+#include <algorithm>
 
-
+#define MAP_SIZE 500
 int main(int argc, char* argv[]) {
   if(argc < 3) {
     std::cout << "Usage : " << argv[0] << " <rootdir> <timeline>" << std::endl
@@ -26,6 +28,22 @@ int main(int argc, char* argv[]) {
 
   auto [tstart, tend] = file.get_time_range();
   std::cout << tstart << ' ' << tend << std::endl;
+
+  std::vector<unsigned char> img(MAP_SIZE);
+  std::fill(img.begin(), img.end(), (unsigned char)255);
+  auto data_ref = cxsom::data::make(file.get_type());
+  for(auto t = tstart; t <= tend; ++t) {
+    file.read(t, data_ref);
+    double at = static_cast<cxsom::data::Scalar&>(*data_ref).value;
+    unsigned int pos = (unsigned int)((MAP_SIZE - 1)*at +.5);
+    img[pos] = (unsigned char)0;
+  }
+
+  {
+    std::ofstream ppm {timeline + "-bmus.ppm"};
+    ppm << "P5\n" << MAP_SIZE << " 1\n255\n";
+    ppm.write((char*)std::data(img), MAP_SIZE);
+  }
       
   return 0;
 }
