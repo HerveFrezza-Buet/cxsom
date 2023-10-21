933e2ee0cd29d2a57408baec3d3a1802e639f9bc
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2023-10-21 13:52:55 +0200

    ADD: 3 examples

--- sked/src/sked.hpp
+++ sked/src/sked.hpp
@@ -4,3 +4,9 @@
 #include <skedCheckings.hpp>
 #include <skedQueue.hpp>
 #include <skedVerbosity.hpp>
+
+/**
+ * @example example-001-001-basic-queue.cpp  
+ * @example example-001-002-ack-queue.cpp 
+ * @example example-001-003-double-buffered-queue.cpp
+ */
