28d5f8d30cdc4f8e26dfc090da0b16d2ea628994
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2024-10-27 15:13:20 +0100

    updt

--- skednet/src/skednetScope.hpp
+++ skednet/src/skednetScope.hpp
@@ -8,14 +8,19 @@ namespace sked {
   namespace net {
     namespace scope {
       namespace xrsw {
+	
 	struct read : public sked::net::protocol::scope::client_plug {
 	  read(std::istream& is, std::ostream& os): sked::net::protocol::scope::client_plug('R', 'r', is, os) {} 
 	};
+	
 	struct write : public sked::net::protocol::scope::client_plug {
 	  write(std::istream& is, std::ostream& os): sked::net::protocol::scope::client_plug('W', 'w', is, os) {} 
 	};
+	
 	struct write_explicit : public sked::net::protocol::scope::client_plug_explicit {
-	  write_explicit(std::istream& is, std::ostream& os): sked::net::protocol::scope::client_plug_explicit('W', 'w', is, os) {} 
+	  write_explicit(std::istream& is, std::ostream& os): sked::net::protocol::scope::client_plug_explicit('W', 'w', is, os) {}
+	};
+	
       }
     }
   }
